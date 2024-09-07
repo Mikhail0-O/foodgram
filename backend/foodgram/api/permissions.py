55f43859519af9edbd916594522336f060ca86db
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-09-07 17:20:13 +0300

    Permissions

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -4,19 +4,14 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        print(request.method)
-        print(request.user.is_authenticated)
         return (
             request.method in permissions.SAFE_METHODS
             or (request.user.is_authenticated)
         )
 
     def has_object_permission(self, request, view, obj):
-        print(request.method)
         if request.method in permissions.SAFE_METHODS:
             return True
-        print(request.method)
         if request.method in ('PATCH', 'DELETE', 'POST'):
-            print(obj.author == request.user)
             return obj.author == request.user
         return False
