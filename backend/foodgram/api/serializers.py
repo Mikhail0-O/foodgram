521feb60944c883d792cae07657f7eb03c301515
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-09-25 23:18:21 +0300

    refac

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -343,6 +343,7 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
             ingredient_instances.append(ingredient_instance)
 
             recipe.ingredients.add(ingredient_instance.id)
+
         IngredientForRecipe.objects.bulk_create(ingredient_instances)
 
     def create(self, validated_data):
