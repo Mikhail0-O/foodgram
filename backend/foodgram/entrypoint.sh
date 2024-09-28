11987f1c38e8c6774fbaa7c4371d02d72e114afa
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-09-28 10:23:01 +0300

    refac

--- backend/foodgram/entrypoint.sh
+++ backend/foodgram/entrypoint.sh
@@ -1,5 +1,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
+cp -r /app/collected_static/. /static/
 
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
